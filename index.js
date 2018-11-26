832b16a2c34088d91e7211d5a8f4f093ab84416f
awmleer <awmleer@sparker.xyz>
2018-11-27 00:04:31 +0800

    first try on yargs

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const app = require('yargs')
+  .usage('$0 <cmd> [args]')
+  .command('hello [name]', 'welcome ter yargs!', (yargs) => {
+    yargs.positional('name', {
+      type: 'string',
+      default: 'Cambi',
+      describe: 'the name to say hello to'
+    })
+  }, function (argv) {
+    console.log('hello', argv.name, 'welcome to yargs!')
+  })
+  .help()
+  .argv
